a5392661aa5e1cd51b40567ae8c12522e1215b79
sterlingking1990 <izundukingsleyemeka@gmail.com>
2024-06-16 22:22:10 +0100

    update ton manifest

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
 
 function App() {
+  const handleConnect = () => {
+    const tonConnectURL =
+      "https://tonhub.com/ton-connect?v=2&id=ec530b57db301d5aeeb0f84ace65f98d9dd07899f3564917a81b11d320740246&r=%7B%22manifestUrl%22%3A%22https%3A%2F%2Fsterlingking1990.github.io%2Fcontract-front-end%2Ftonconnect-manifest.json%22%2C%22items%22%3A%5B%7B%22name%22%3A%22ton_addr%22%7D%5D%7D&ret=googlechrome%3A%2F%2F";
+
+    // Open the TonConnect URL to initiate the connection
+    window.open(tonConnectURL, "_blank");
+  };
+
   const {
     contract_address,
     contract_balance,
@@ -18,7 +26,7 @@ function App() {
   return (
     <div>
       <div>
-        <TonConnectButton />
+        <TonConnectButton onClick={handleConnect} />
       </div>
       <div>
         <div className="Card">
